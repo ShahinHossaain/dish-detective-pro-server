448ca4095bd37dc9e45e7c6fdd56ad8fe88aa5ed
ShahinHossaain <mdshahinhossaain@gmail.com>
2023-05-02 22:19:43 +0600

    img add

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express')
+const cors = require('cors');
 const app = express()
-const port = 3000
+const port = 3001
+
+app.use(cors())
 
 const chefs = require('./chefs.json')
 app.get('/', (req, res) => {
